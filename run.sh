7fda5f4befd7ce70406140bbe46f317223dff75a
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2024-03-17 11:30:55 +0900

    Add tags

--- run.sh
+++ run.sh
@@ -7,3 +7,19 @@ cat <<EOF
 
 EOF
 (cd server1; ansible-playbook -i ../hosts main.yml -e main_foo=foo)
+
+cat <<EOF
+----------------------------------------
+  server1 tag1
+----------------------------------------
+
+EOF
+(cd server1; ansible-playbook -i ../hosts main.yml -e main_foo=foo -t tag1)
+
+cat <<EOF
+----------------------------------------
+  server1 tag2
+----------------------------------------
+
+EOF
+(cd server1; ansible-playbook -i ../hosts main.yml -e main_foo=foo -t tag2)
